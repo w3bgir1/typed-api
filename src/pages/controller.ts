0bd203038bfbf72c9017100fe609e6e7ec2e8701
w3bgir1 <akulshyna@gmail.com>
2019-03-19 11:33:55 +0100

    added put and post request to pages

--- src/pages/controller.ts
+++ src/pages/controller.ts
@@ -1,21 +1,38 @@
-import { JsonController, Get, Param } from 'routing-controllers'
-import pagesById, { Page } from './data'
+import {
+  JsonController,
+  Get,
+  Param,
+  Put,
+  Body,
+  Post,
+  HttpCode
+} from "routing-controllers";
+import pagesById, { Page } from "./data";
 
-type PageList = { pages: Page[] }
+type PageList = { pages: Page[] };
 
 @JsonController()
 export default class PageController {
+  @Get("/pages/:id")
+  getPage(@Param("id") id: number): Page {
+    return pagesById[id];
+  }
 
-    @Get('/pages/:id')
-    getPage(
-        @Param('id') id: number
-    ): Page {
-        return pagesById[id]
-    }
+  @Get("/pages")
+  allPages(): PageList {
+    return { pages: Object.values(pagesById).map(value => value) };
+  }
 
-    @Get('/pages')
-    allPages(): PageList {
-        return {pages: Object.values(pagesById).map(value => value)}
-    }
+  @Put("/pages/:id")
+  updatePage(@Param("id") id: number, @Body() body: Partial<Page>): Page {
+    console.log(`Incoming PUT body param:`, body);
+    return pagesById[id];
+  }
+
+  @Post("/pages")
+  @HttpCode(201)
+  createPage(@Body() body: Page): Page {
+    console.log(`Incoming POST body param:`, body);
+    return body;
+  }
 }
-
